12e7dd706ea43026d43ceb5de9ca4ac47c7cf1b6
zeinab-sheikhi <zeinab.sheykhie@gmail.com>
2025-01-19 14:31:01 +0100

    engine contains both tanh and relu which are modified based on the exp and pow operators

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -2,115 +2,128 @@ import math
 
 
 class Value:
-
-    """ stores a single scalar value and its gradient """
-
-    def __init__(self, data, _children=(), _op='', label=''):
+    def __init__(self, data, children=(), op='', label=''):
         self.data = data
         self.grad = 0.0
-        self._prev = set(_children) 
-        self._op = _op
         self.label = label
+        self._prev = set(children)
+        self._op = op
         self._backward = lambda: None
-          
+
+    def __repr__(self):
+        return f"Valeu(data={self.data}, grad={self.grad})"
+
     def __add__(self, other):
-        other = other if isinstance(other, Value) else Value(other) 
-        out = Value(self.data + other.data, (self, other), '+')
-        
+        other = other if isinstance(other, Value) else Value(data=other)
+        out = Value(
+            data=self.data + other.data,
+            label='+',
+            children=(self, other)
+        )
+
         def _backward():
             self.grad += out.grad
             other.grad += out.grad
-        
         out._backward = _backward
         return out
-    
-    def __pow__(self, other):
-        assert isinstance(other, (int, float)), "only support int/float powers"
-        out = Value(self.data ** other, (self,), f'**{other}')
 
-        def _backward():
-            self.grad += other * self.data ** (other - 1) * out.grad
-
-        out._backward = _backward
-        return out
-    
     def __mul__(self, other):
-        other = other if isinstance(other, Value) else Value(other) 
-        out = Value(self.data * other.data, (self, other), '*')
-        
+        other = other if isinstance(other, Value) else Value(data=other)
+        out = Value(
+            data=self.data * other.data,
+            label='*',
+            children=(self, other)
+        )
+
         def _backward():
             self.grad += other.data * out.grad
             other.grad += self.data * out.grad
-
         out._backward = _backward
         return out
-    
-    def tanh(self):
-        n = self.data
-        t = (math.exp(2*n) - 1) / (math.exp(2*n) + 1)
-        out = Value(t, (self, ), 'tanh')
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "Only int/float data types are supported!"
+        out = Value(
+            data=self.data ** other,
+            label=f'**{other}',
+            children=(self, )
+        )
 
         def _backward():
-            self.grad += (1 - t ** 2) * out.grad
-        
+            self.grad += out.grad * (other * (self.data) ** (other - 1))
         out._backward = _backward
         return out
-    
+
     def relu(self):
-        out = Value(0 if self.data < 0 else self.data, (self, ), 'ReLU')
-        
+        out = Value(
+            data=self.data if self.data > 0 else 0,
+            label='ReLU',
+            children=(self, )
+        )
+
         def _backward():
             self.grad += (self.data > 0) * out.grad
-        
         out._backward = _backward
         return out
 
     def exp(self):
-        x = self.data
-        out = Value(math.exp(x), (self,), 'exp')
+        out = Value(
+            data=math.exp(self.data),
+            label='exp',
+            children=(self, )
+        )
 
         def _backward():
-            self.grad += out.data * out.grad
-        
+            self.grad += out.grad * out
+        out._backward = _backward
+        return out
+
+    def tanh(self):
+        exp_2n = (self * 2).exp()
+        out = Value(
+            data=(exp_2n - 1) / (exp_2n + 1),
+            label='tanh',
+            children=(self, )
+        )
+
+        def _backward():
+            self.grad += out.grad * (1 - out ** 2)
         out._backward = _backward
         return out
 
     def backward(self):
-        # topological order all of the children in the graph
-        
         topo = []
         visited = set()
-        
+
         def build_topo(v):
             if v not in visited:
                 visited.add(v)
                 for child in v._prev:
                     build_topo(child)
                 topo.append(v)
-            return topo
-        
+
         build_topo(self)
-        self.grad = 1.0
+        self.grad = 1
         for node in reversed(topo):
             node._backward()
-    
-    def __rmul__(self, other):
-        return self * other
-    
-    def __radd__(self, other):
-        return self + other
-    
+
     def __neg__(self):
         return self * -1
-    
+
+    def __radd__(self, other):
+        return self + other
+
     def __sub__(self, other):
         return self + (-other)
-    
+
     def __rsub__(self, other):
         return other + (-self)
 
+    def __rmul__(self, other):
+        return self * other
+
     def __truediv__(self, other):
-        return self * other ** -1
-    
-    def __repr__(self):
-        return f"Value(data={self.data}, grad={self.grad})"    
+        return self * (other ** -1)
+
+    def __rtruediv__(self, other):
+        return other * (self ** -1)
--- micrograd/nn.py
+++ micrograd/nn.py
@@ -1,44 +1,64 @@
 import random
 
 from engine import Value
+from typing import List
 
 
-class Neuron:
-    
-    def __init__(self, nin):
-        self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
-        self.b = Value(random.uniform(-1, 1))
-    
-    def __call__(self, x): 
-        act = sum((wi * xi for wi, xi in zip(self.w, x)), self.b)
-        out = act.tanh()
-        return out
-    
+class Module:
+    def zero_grad(self):
+        for p in self.parameters():
+            p.grad = 0
+
     def parameters(self):
-        return [self.b] + self.w
-        
-         
-class Layer:
-    def __init__(self, nin, nout):
-        self.neurons = [Neuron(nin) for _ in range(nout)]
-    
+        return []
+
+
+class Neuron(Module):
+    def __init__(self, num_inputs, nonlin=True):
+        self.weight = [Value(data=random.uniform(-1, 1)) for _ in range(num_inputs)]
+        self.bias = Value(0)
+        self.nonlin = nonlin
+
     def __call__(self, x):
-        outs = [neuron(x) for neuron in self.neurons]
-        return outs[0] if len(outs) == 1 else outs
-    
+        act = sum([wi * xi for xi, wi in zip(x, self.weight)], self.bias)
+        return act.relu() if self.nonlin else act
+
+    def parameters(self):
+        return self.weight + [self.bias]
+
+    def __repr__(self):
+        return f"{'ReLU' if self.nonlin else 'Linear'} Neuron({len(self.weight)})"
+
+
+class Layer(Module):
+    def __init__(self, n_in: int, n_out: int):
+        self.neurons = [Neuron(n_in) for _ in range(n_out)]
+
+    def __call__(self, x):
+        out = [neuron(x) for neuron in self.neurons]
+        return out[0] if len(out) == 1 else out
+
     def parameters(self):
         return [p for neuron in self.neurons for p in neuron.parameters()]
 
+    def __repr__(self):
+        return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
 
-class MLP:
-    def __init__(self, nin, nouts):
-        sz = [nin] + nouts
-        self.layers = [Layer(sz[i], sz[i + 1]) for i in range(len(nouts))]
-    
+
+class MLP(Module):
+    def __init__(self, n_in: int, n_outs: List[int]):
+        sz = [n_in] + n_outs
+        self.layers = [
+            Layer(n_in=sz[i], n_out=sz[i + 1], nonlin=i != len(n_outs) - 1) 
+            for i in range(sz)]
+
     def __call__(self, x):
         for layer in self.layers:
             x = layer(x)
-        return x  
+        return x
 
     def parameters(self):
-        return [p for layer in self.layers for p in layer.parameters()]      
+        return [p for layer in self.layers for p in layer.parameters()]
+
+    def __repr__(self):
+        return f"MLP of [{', '.join(str(layer) for layer in self.layers)}]"
